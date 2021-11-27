bcba731e1b414207b117d24e686c40e236abd7fc
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-11-27 10:05:25 +0530

    TODO 2: Adding spring-data-jpa dependency and checking if the bean is getting created

--- homework/src/main/java/com/upgrad/homework/HomeworkApplication.java
+++ homework/src/main/java/com/upgrad/homework/HomeworkApplication.java
@@ -2,12 +2,17 @@ package com.upgrad.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomeworkApplication.class, args);
+
+	ApplicationContext context = SpringApplication.run(HomeworkApplication.class, args);
+	DataSource source = context.getBean(DataSource.class) ;
 	}
 
 }
